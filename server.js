dccefa25df3966a6387b7cc9767c486476e20b7f
shaundraws <mail@shaundraws.com>
2020-06-06 11:03:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => {
-  io.emit('time', new Date().toTimeString()), 1000);
- });
-
-
 var countdown = 1000;
 setInterval(function() {
   countdown--;
